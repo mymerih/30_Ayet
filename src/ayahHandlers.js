592c7b854f1a8a53ff16ddb624dfcd4b68cc9a2b
mymerih <mymerih@gmail.com>
2024-12-09 15:05:01 +0100

    module v1.3 next'i sayan ve ayet atlatma yapildi. Next'e basinca ayni ayet 2 kez caliniyor.

--- src/ayahHandlers.js
+++ src/ayahHandlers.js
@@ -4,8 +4,6 @@ import * as domHandlers from "./domHandlers.js";
 import { TextScramble } from "./TextScramble.js";
 import { ayatList } from "../data/ayahList.js";
 
-let currentIndex = 0;
-
 // Initialize TextScramble with DOM elements
 const {
   baslikContainer,
@@ -16,6 +14,11 @@ const {
   nextBtn,
   prevBtn,
 } = domHandlers.getElements();
+
+// Global degiskenler
+let currentIndex = 0; // Index of Ayat
+let isAnimating = false; // Kontrol degiskeni. Animasyon devam ederken diger ayete gecisi onlemek icin
+let counter = 0; // Ayet atlama adedi.
 
 const fx = new TextScramble(mealContainer);
 
@@ -49,20 +52,46 @@ async function displayAyah(index) {
 
 // Function to handle 'Next Ayah' logic
 async function handleNextAyah(skipAnimation = false) {
-    if (!skipAnimation) {
-        // Wait for the current ayah's animation to complete before moving to the next
-        await displayAyah(currentIndex);
-    }
-    
-    // Move to the next Ayah
-    currentIndex = (currentIndex + 1) % ayatList.length;
+  counter = counter + 1;
 
-  // Display the next Ayah
-  displayAyah(currentIndex);
+  if (isAnimating) {
+    console.warn("Animasyon devam ediyor, lütfen bekleyin.");
+    return;
+  }
+
+  try {
+    isAnimating = true; // Animasyon basliyor. Animasyon bitene kadar bu kismi tekrar calistirma.
+
+    if (!skipAnimation) {
+      // Wait for the current ayah's animation to complete before moving to the next
+      await displayAyah(currentIndex);
+    }
+
+    // Move to the next Ayah
+    if (counter > 0) {
+      currentIndex = (currentIndex + counter) % ayatList.length;
+      counter = 0;
+    } else {
+      currentIndex = (currentIndex + 1) % ayatList.length;
+    }
+    // Display the next Ayah
+    displayAyah(currentIndex);
+  } catch (error) {
+    console.error("Bir hata olustu. Hata: ", error);
+  } finally {
+    isAnimating = false; // Islem tammamlandi, kilidi kaldir.
+  }
 }
 
 // Function to handle 'Previous Ayah' logic
 async function handlePrevAyah() {
+  counter = counter -1;
+
+  if (isAnimating) {
+    console.warn("Animasyon devam ediyor, lütfen bekleyin.");
+    return;
+  }
+
   // Wait for the current ayah's animation to complete
   await displayAyah(currentIndex);
 
